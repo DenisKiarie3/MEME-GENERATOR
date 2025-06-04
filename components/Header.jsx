992453d68d8223792fa9b1231ec4daaaf0232561
Denis Kiarie <deniskiarie3@gmail.com>
2025-06-04 12:25:34 +0300

    Added and styled the Header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,9 @@
+import Troll2 from "../images/Troll2.png"
 export default function Header() {
     return(
-        <h1>Header</h1>
+        <header className="flex items-center h-[65px] bg-gradient-to-r from-[#672280] to-[#A626D3] text-white p-[20px]">
+            <img className="w-[40px] h-[40px] mr-[6px]" src={Troll2} alt="Meme Generator Icon"/>
+            <h1 className="text-[1.25rem] m-auto">Meme Generator</h1>
+        </header>
     )
 }
